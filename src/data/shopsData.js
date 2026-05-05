4952500952bcba08112d423b875f12217232bdbe
Rosa Lina <rofr0001@stud.kea.dk>
2026-05-05 14:41:58 +0200

    add all content for shops page

---
+++ src/data/shopsData.js
@@ -0,0 +1,48 @@
+export const areas = [
+  "reffen",
+  "city",
+  "østerbro",
+  "nørrebro",
+  "vesterbro",
+  "frederiksberg",
+  "islands brygge",
+];
+
+export const shops = [
+  {
+    title: "carlsbergbyen",
+    imgSrc: "/assets/img/shops/carlsbergbyen.png",
+    street: "Franciska Clausens Plads 29",
+    town: "Copenhagen V, DK-1799",
+    area: "vesterbro",
+    openHours1: "Open every day",
+    openHours2: "07:30-17:00",
+  },
+  {
+    title: "frederiksberg",
+    imgSrc: "/assets/img/shops/frb.png",
+    street: "Gammel Kongevej 109",
+    town: "Frederiksberg, DK-1850",
+    area: "frederiksberg",
+    openHours1: "Open every day",
+    openHours2: "07:30-18:00",
+  },
+  {
+    title: "kødbyen",
+    imgSrc: "/assets/img/shops/koedbyen.png",
+    street: "Høkerboderne 17",
+    town: "Copenhagen V, DK-1712",
+    area: "vesterbro",
+    openHours1: "Open every day",
+    openHours2: "07:30-18:00",
+  },
+  {
+    title: "istedgade",
+    imgSrc: "/assets/img/shops/istedgade.png",
+    street: "Istedgade 61",
+    town: "Copenhagen V, DK-1716",
+    area: "vesterbro",
+    openHours1: "Open every day",
+    openHours2: "07:30-18:00",
+  },
+];
